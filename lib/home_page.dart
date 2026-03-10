0467e05a68ad1eef91004765701bdad4d7bace74
saiteja.k <saiteja.k@cgg.gov.in>
2026-03-10 14:53:43 +0530

    added workflows ci1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,8 +16,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
-    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    // final counterCubit = BlocProvider.of<CounterCubit>(context);
+    // final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
--- lib/inc_dec.dart
+++ lib/inc_dec.dart
@@ -8,7 +8,7 @@ class IncDec extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
+  //  final counterCubit = BlocProvider.of<CounterCubit>(context);
     final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(),
