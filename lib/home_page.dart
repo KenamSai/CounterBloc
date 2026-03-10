f3c22683ee5d3ac91c74017dd8757ea9d71327fa
saiteja.k <saiteja.k@cgg.gov.in>
2026-03-10 14:57:32 +0530

    added workflows ci2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:counterappblocpackg/bloc/counter_bloc.dart';
-import 'package:counterappblocpackg/cubit/counter_cubit.dart';
+//import 'package:counterappblocpackg/cubit/counter_cubit.dart';
 import 'package:counterappblocpackg/inc_dec.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/inc_dec.dart
+++ lib/inc_dec.dart
@@ -1,5 +1,5 @@
 import 'package:counterappblocpackg/bloc/counter_bloc.dart';
-import 'package:counterappblocpackg/cubit/counter_cubit.dart';
+//import 'package:counterappblocpackg/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
